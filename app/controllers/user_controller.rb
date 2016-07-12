6b6faac2771fd14bf0d6232ccacf9a924739c163
AiswaryaLakshmy <aiswarya@7nodes.com>
2016-07-12 11:15:13 +0530

    Created login page and sign up page

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -14,6 +14,16 @@ class UserController < ApplicationController
 		redirect_to user_index_path
 	end
 
+	def signup
+		@user = User.new(user_params)
+
+		if @user.save
+			redirect_to new_user_path
+		else
+			render 'index'
+		end
+	end
+
 	private
 	def user_params
 		params.require(:user).permit(:username, :password)
